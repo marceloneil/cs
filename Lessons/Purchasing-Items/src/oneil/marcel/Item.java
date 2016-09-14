f308698386877d31360cb2c4b48234ccfbf76820
Marcel O'Neil <marcel@marceloneil.com>
2016-09-13 20:26:54 -0400

    Update Item constructor

--- Lessons/Purchasing-Items/src/oneil/marcel/Item.java
+++ Lessons/Purchasing-Items/src/oneil/marcel/Item.java
@@ -6,7 +6,7 @@ public class Item {
 	private int quantity;
 	private double salesTax = 1.13;
 
-	Item(String name, double price, int quantity) {
+	public Item(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
