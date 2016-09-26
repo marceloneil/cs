125c4420ff05cfb9f330d639e6b7b2723b2aba16
Marcel O'Neil <marcel@marceloneil.com>
2016-09-25 22:09:10 -0400

    Hacker Challenge

--- Lessons/Dice-Roller/src/oneil/marcel/Driver.java
+++ Lessons/Dice-Roller/src/oneil/marcel/Driver.java
@@ -3,14 +3,23 @@ package oneil.marcel;
 public class Driver {
 
 	public static void main(String[] args) {
-		Die firstDie = new Die(6);
-		Die secondDie = new Die(6);
+		Die die1 = new Die(6);
+		Die die2 = new Die(6);
+		int[] rolls = new int[11];
 		
-		firstDie.roll();
-		secondDie.roll();
-		int total = firstDie.getValue() + secondDie.getValue();
+		for(int i = 0; i < 1000; i++){
+			die1.roll();
+			die2.roll();
+			int total = die1.getValue() + die2.getValue();
+			rolls[total-2] ++;
+		}
 		
-		System.out.println("Dice roll was " + total);
+		for(int i = 0; i < 11; i++){
+			System.out.println("Roll sum " + (i+2) + ": " + rolls[i]);
+		}
+		System.out.println("I found " + rolls[5] + " rolls with a sum of 7");
+		System.out.println("The theoretical probability is 16.7%");
+		System.out.printf("The experimental probability is %.1f%%\n", (float) rolls[5] / 10);
 	}
 
 }
